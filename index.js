df304636ffe1c62195db52e941cd009510eb932d
pos0637 <pos0637@163.com>
2020-12-09 15:14:26 +0800

    添加TypeScript支持。

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import TestView from './src/test/TestView';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TestView);
